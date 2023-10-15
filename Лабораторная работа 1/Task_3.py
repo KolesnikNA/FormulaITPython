7c79827f86bea3d81307e5cfc67fa33cb87cb6f2
KolesnikNA <147950753+KolesnikNA@users.noreply.github.com>
2023-10-15 13:16:29 +0300

    Update Task_3.py
    Код сокращен операцией "деление нацело" вместо приведения типов и использования дополнительной переменной

--- Лабораторная работа 1/Task_3.py
+++ Лабораторная работа 1/Task_3.py
@@ -1,9 +1,8 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-quantity_of_players = len(list_players)
-to_equal_team = int(quantity_of_players/2)
 
-first_team = list_players[:to_equal_team:]
-second_team = list_players[to_equal_team::]
+to_equal_team = len(list_players) // 2
+first_team = list_players[:to_equal_team]
+second_team = list_players[to_equal_team:]
 
 print(first_team)
 print(second_team)
